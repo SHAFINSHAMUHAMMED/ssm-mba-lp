6a7d6bcdef8b223556e67a3b51567aaf2060f1bd
SHAFINSHAMUHAMMED <122475859+SHAFINSHAMUHAMMED@users.noreply.github.com>
2023-11-15 09:52:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header/header'
+import Hero from './components/Hero/hero'
+function App() {
+  return (
+    <div className='body'>
+      <Header/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Hero/hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function hero() {
+  return (
+    <div className='hero-section bg-backgrounds-wisdomWhite'>
+      <div className='hero-sub flex justify-around'>
+            <div className='hero-heading'>
+                <div className='sub-heading'>
+                    <img src="\images\svg\target.svg" alt="" />
+                    <h4 className='font-bold'>Ready to launch the career & life you've always wanted?</h4>
+                </div>
+            </div>
+            <div>image</div>
+      </div>
+    </div>
+  )
+}
+
+export default hero
